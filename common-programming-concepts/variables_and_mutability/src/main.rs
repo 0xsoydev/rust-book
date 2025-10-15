182a595dd11e1cb075729ad297183325ec72b448
0xsoydev <0x00zer0day@gmail.com>
2025-10-16 00:43:45 +0530

    variables and their mutability

---
+++ common-programming-concepts/variables_and_mutability/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+
+    //constant declarations
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    // mutability
+    let mut x = 5;
+    println!("The value of x is : {x}");
+    x = 6;
+
+    //shadowing (reuse of x with declaration of 'let')
+    let x = x + 1
+
+    //change of scope
+    {
+        let x = x * 2;
+        println!("The value of x is : {x}");
+    }
+
+    println!("The value of x is : {x}");
+
+    let mut spaces = "     ";
+    let spaces = spaces.len();
+}
