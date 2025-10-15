670d77aa6136a51d692d262b8053fe9be9851ef0
0xsoydev <0x00zer0day@gmail.com>
2025-10-16 00:14:46 +0530

    guessing game with loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,38 @@
 use std::io;
+// use rand::Rng;
+use rand;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
+    // let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = rand::random_range(1..=100);
+    println!("The secret number is {secret_number}");
 
-    let mut guess = String::new();
+    // let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
 
-    println!("You guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too less!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal =>  {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
