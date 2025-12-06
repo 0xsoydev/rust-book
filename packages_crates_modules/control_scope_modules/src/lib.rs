4cfaee5c51f16f1c2a017ae44dbb1ef7e51bd577
0xsoydev <0x00zer0day@gmail.com>
2025-12-06 16:39:57 +0530

    Control Scope Modules

--- packages_crates_modules/control_scope_modules/src/lib.rs
+++ packages_crates_modules/control_scope_modules/src/lib.rs
@@ -31,6 +31,23 @@ pub mod back_of_house {
     }
 }
 
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+}
+
+use crate::front_of_house::hosting;
+
+mod customer {
+    use crate::front_of_house::hosting; //This statement is necessary in order for
+                                        //hosting::add_to_waitlist() to work below
+
+    pub fn eat_at_restaurant() {
+        hosting::add_to_waitlist(); //Errors out since crate initialization wasn't declared here
+    }
+}
+
 //pub fn eat_at_restaurant() {
 //    let mut meal = back_of_house::Breakfast::summer("Rye");
 //    meal.toast = String::from("Wheat");
